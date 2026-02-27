1fd650a8451d55956fc878d04891f12f2011d0de
alam <developerimthiyas+imthiyas@gmail.com>
2026-02-27 06:56:41 +0530

    GlasscardOverlay removed from index

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,19 +5,15 @@ import { Layout } from '../components/layout/Layout';
 import HomeScreen from '../screens/HomeScreen';
 import { NavigationProvider } from './navigation/NavigationProvider';
 
-import GlassCardOverlay from '../components/ui/GlassCardOverlay';
 
 
 const App: React.FC = () => {
   return (
     <ThemeProvider>
       <NavigationProvider>
-        <>
-          <GlassCardOverlay />
-          <Layout>
-            <HomeScreen />
-          </Layout>
-        </>
+        <Layout>
+          <HomeScreen />
+        </Layout>
       </NavigationProvider>
     </ThemeProvider>
   );
